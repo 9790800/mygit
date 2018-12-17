0383e3bfc04982d3d052f36e7df137899e05560e
troll7788 <you@example.com>
2018-12-17 19:42:44 +1000

    new ts.py

---
+++ ts.py
@@ -0,0 +1,58 @@
+import paramiko
+import getpass
+
+user = raw_input("User: ")
+secret = getpass.getpass("Password: ")
+hostlbcore = raw_input("IP LBcore: ")
+hostlbarcd = raw_input("IP LBarcd: ")
+hostlbbd = raw_input("IP LBbd: ")
+hostlbtest = raw_input("IP LBtest: ")
+port = 22
+
+
+def sshsess(comand, host):
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(hostname=host, username=user, password=secret, port=port)
+	stdin, stdout, stderr = client.exec_command(comand)
+	data = stdout.read() + stderr.read()
+	client.close()
+	return data
+
+
+#LBcore
+host = hostlbcore
+
+comand = " rpm -qa | egrep -i 'lbcore-2|lbtv-2|lbphone-2' "
+
+outsshlbcore = sshsess(comand, host)
+print '\033[91m' + '\n rpm LBcore ' + host +'\n' + '\033[0m'
+print '\033[96m' + outsshlbcore + '\033[0m'
+
+########
+
+#LBarcd
+host = hostlbarcd
+
+comand = " rpm -qa | egrep -i 'lbarcd-2' "
+
+outsshlbarcd = sshsess(comand, host)
+print '\033[91m' + '\n rpm LBarcd ' + host +'\n' + '\033[0m' 
+print '\033[96m' + outsshlbarcd + '\033[0m'
+
+########
+
+#LBbd
+host = hostlbbd
+
+comand = "sudo find /opt/backup -name '*.bz2' "
+
+outsshlbbd = sshsess(comand, host)
+print '\033[91m' + '\n BD LBbd ' + host +'\n' + '\033[0m' 
+print '\033[96m' + outsshlbbd + '\033[0m'
+
+########
+key = 'no'
+key =raw_input("Download? (yes/no): ")
+if ( key == 'yes' ): print 'Download...'
+
